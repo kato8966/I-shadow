96258ffc032c0b892faf9d1271eae5b04e407cdd
Kato Taisei <66937409+kato8966@users.noreply.github.com>
2025-02-15 18:05:12 +0900

    create shadowing_frame

--- i-shadow.py
+++ i-shadow.py
@@ -1,5 +1,11 @@
 from tkinter import PhotoImage, Text, Tk, ttk
 
+
+def start_shadowing():
+    start_frame.grid_remove()
+    shadowing_frame.grid(row=0, column=0, sticky='nwes')
+
+
 root = Tk()
 root.geometry('600x600')
 root.title('I-shadow')
@@ -9,7 +15,8 @@ root.columnconfigure(0, weight=1)
 start_frame = ttk.Frame(root)
 start_frame.grid(row=0, column=0, sticky='nwes')
 mic_off_icon = PhotoImage(file='icon/mic_off.png')
-start_button = ttk.Button(start_frame, image=mic_off_icon)
+start_button = ttk.Button(start_frame, image=mic_off_icon,
+                          command=start_shadowing)
 start_button.grid(row=0, column=0, columnspan=2)
 notice = ttk.Label(start_frame, text='The icon is designed by SumberRejeki '
                                      'from Flaticon')
@@ -27,5 +34,17 @@ xs.grid(row=3, column=0, sticky='we')
 ys.grid(row=2, column=1, sticky='ns')
 start_frame.grid_rowconfigure(2, weight=1)
 start_frame.grid_columnconfigure(0, weight=1)
+
+shadowing_frame = ttk.Frame(root)
+mic_on_icon = PhotoImage(file='icon/mic_on.png')
+start_button = ttk.Button(shadowing_frame, image=mic_on_icon)
+start_button.grid(row=0, column=0)
+notice = ttk.Label(shadowing_frame, text='The icon is designed by '
+                                         'SumberRejeki from Flaticon')
+notice.grid(row=1, column=0)
+shadowing_text = Text(shadowing_frame, wrap='word', state='disabled')
+shadowing_text.grid(row=2, column=0, sticky='nwes')
+shadowing_frame.grid_rowconfigure(2, weight=1)
+shadowing_frame.grid_columnconfigure(0, weight=1)
 
 root.mainloop()
