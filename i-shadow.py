7e75ac5950996c33c5ff746946b226bff780ffaa
Kato Taisei <66937409+kato8966@users.noreply.github.com>
2025-02-22 10:45:15 +0900

    finish shadowing and show result

--- i-shadow.py
+++ i-shadow.py
@@ -4,7 +4,7 @@ import operator
 from collections import Counter
 from copy import copy
 from queue import Queue
-from tkinter import PhotoImage, Text, Tk, ttk
+from tkinter import PhotoImage, StringVar, Text, Tk, ttk
 
 import nltk
 import sounddevice as sd
@@ -39,6 +39,42 @@ def start_shadowing():
     root.attributes("-topmost", 1)
 
 
+def calculate_f1_score():
+    p = (true_positives / total_user_words if total_user_words > 0
+         else 0)
+    r = (true_positives / total_captions_words if total_captions_words > 0
+         else 0)
+    f1 = (2 * p * r) / (p + r) if p + r > 0 else 0
+
+    p = round(p * 100)
+    r = round(r * 100)
+    f1 = round(f1 * 100)
+    if true_positives != total_user_words:
+        p = min(p, 99)
+        f1 = min(f1, 99)
+    if true_positives != total_captions_words:
+        r = min(r, 99)
+        f1 = min(f1, 99)
+    return p, r, f1
+
+
+def show_result():
+    result_frame.grid(row=0, column=0, sticky='nwe')
+    p, r, f1 = calculate_f1_score()
+    pr_result_content.set(f'Precision: {p}\nRecall: {r}\n'
+                          'Your F1 score is ...\n')
+    f1_score.set(str(f1))
+
+
+def finish_shadowing():
+    global is_shadowing
+    is_shadowing = False
+    shadowing_frame.grid_remove()
+
+    if audio_queue.empty():
+        show_result()
+
+
 audio_queue = Queue()
 last_temp_result = ''
 true_positives = 0
@@ -52,7 +88,9 @@ def callback_audio_in(event):
         result = json.loads(recognizer.Result())['text']
     else:
         result = json.loads(recognizer.PartialResult())['partial']
-        if result == last_temp_result:
+        if not is_shadowing and audio_queue.empty():
+            is_final = True
+        elif result == last_temp_result:
             return
     split_result = result.split()
     shadowing_text['state'] = 'normal'
@@ -77,6 +115,9 @@ def callback_audio_in(event):
     shadowing_text['state'] = 'disabled'
     last_temp_result = '' if is_final else result
     shadowing_text.see('end')
+
+    if not is_shadowing and audio_queue.empty():
+        show_result()
 
 
 root = Tk()
@@ -111,8 +152,9 @@ start_frame.grid_columnconfigure(0, weight=1)
 is_shadowing = False
 shadowing_frame = ttk.Frame(root)
 mic_on_icon = PhotoImage(file='icon/mic_on.png')
-start_button = ttk.Button(shadowing_frame, image=mic_on_icon)
-start_button.grid(row=0, column=0)
+stop_button = ttk.Button(shadowing_frame, image=mic_on_icon,
+                         command=finish_shadowing)
+stop_button.grid(row=0, column=0)
 notice = ttk.Label(shadowing_frame, text='The icon is designed by '
                                          'SumberRejeki from Flaticon')
 notice.grid(row=1, column=0)
@@ -124,6 +166,16 @@ shadowing_text.tag_configure('partial_incorrect', foreground='#FFCCCC')
 shadowing_text.grid(row=2, column=0, sticky='nwes')
 shadowing_frame.grid_rowconfigure(2, weight=1)
 shadowing_frame.grid_columnconfigure(0, weight=1)
+
+result_frame = ttk.Frame(root)
+pr_result_content = StringVar()
+pr_result = ttk.Label(result_frame, textvariable=pr_result_content)
+f1_score = StringVar()
+f1_score_label = ttk.Label(result_frame, textvariable=f1_score)
+pr_result.grid(row=0, column=0)
+f1_score_label.grid(row=1, column=0)
+result_frame.grid_rowconfigure(1, weight=1)
+result_frame.grid_columnconfigure(0, weight=1)
 
 model = vosk.Model(lang='en-us')
 samplerate = sd.query_devices(kind='input')['default_samplerate']
